7066acc87a17832e6fa554da191dfffd264e8748
marcos reis <marcos.reis349@gmail.com>
2023-10-04 11:48:30 -0300

    chore: install typeorm

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+
+const PORT = process.env.PORT || 8080
+
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Tudo nosso!")
+})
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`)
+})
